b26258701f800ca31816eec96965f9bb7d011e25
Gael Magnan de bornier <gdebornier@sewan.eu>
2014-12-16 14:50:21 +0100

    Added a new Task to redeploy your sources automatically. Usefull for post-receive hooks on PyGitHook servers. Added a execute piped function in Bash.py

---
+++ src/Tasks/DeployMasterTask.py
@@ -0,0 +1,31 @@
+"""
+Hook task to deploy the last version of master.
+
+AUTHOR:
+    Gael Magnan de bornier
+"""
+
+from src.Tasks.HookTask import HookTask
+from src.Utils.Bash import execute_piped_command
+
+class DeployMasterTask(HookTask):
+    """
+    Hook task to deploy the last version of master.
+    """
+
+    def execute(self, refname, to="/var/PyGitHook/", **kwargs):
+        if refname != "refs/heads/master":
+            print("This branch({0}) is not master, nothing to do.".format(refname))
+            return True
+
+        command1 = "git archive --format=tar master"
+        command2 = "tar xf - -C %s" % to
+        ret_code, output = execute_piped_command(command1=command1, command2=command2)
+        if ret_code:
+            print("Error getting the archive from master. Contact your administrator.")
+            print("command:{0} | {1}".format(command1, command2))
+            print("ret_code:{0} output:{1}".format(ret_code, "\n".join(output)))
+            return False
+
+        print("master has been successfully deployed in {0}".format(to))
+        return True
--- src/Utils/Bash.py
+++ src/Utils/Bash.py
@@ -7,24 +7,46 @@ AUTHOR:
     Gael Magnan de bornier
 """
 
-from subprocess import check_output, CalledProcessError, STDOUT
+from subprocess import check_output, CalledProcessError, STDOUT, PIPE, Popen
 
-def execute_command(command):
+def execute_command(command, shell=False):
     """Execute a bash command,
     returns a return code 0 for success the error code otherwise
     and the output as a array of string (1 element = 1 line)"""
     try:
         ret_code = 0
         try:
-            output = check_output(command.split(), stderr=STDOUT)
+            output = check_output(command.split(), stderr=STDOUT, shell=shell)
         except CalledProcessError, error:
             output = error.output
             ret_code = error.returncode
-        res = output.split('\n')
-        return ret_code, res
+        return ret_code, output.split('\n')
     except StandardError, error:
         print("The command: %s failed.\n"
               "Please transmit the following message to your administrator:" %
               command)
         print(error)
+    return 1, []
+
+
+def execute_piped_command(command1, command2):
+
+    try:
+        p1 = Popen(command1.split(), stdout=PIPE)
+    except CalledProcessError, error:
+        return error.returncode, error.output.split('\n')
+    else:
+        try:
+            p2 = Popen(command2.split(), stdin=p1.stdout, stderr=STDOUT )
+        except CalledProcessError, error:
+            return error.returncode, error.output.split('\n')
+        else:
+            try:
+                p1.stdout.close()
+                output = p2.communicate()[0]
+                return 0, output.split('\n')
+            except StandardError, error:
+                print("The commands: {0} failed.\n".format((command1, command2)))
+                print("Please transmit the following message to your administrator:")
+                print(error)
     return 1, []
